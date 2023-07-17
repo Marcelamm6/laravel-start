1f5efd02a0096f315f11553e07fe883f2eedc7ba
marcelamm6@gmail.com <marcelamm6@gmail.com>
2023-07-17 14:21:04 -0300

    Post-Policy-login

--- ourmainapp/routes/web.php
+++ ourmainapp/routes/web.php
@@ -18,13 +18,17 @@ use App\Http\Controllers\UserController;
 
 
 //User Routes
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login');
 
-Route::post('/register', [UserController::class,'register']);
+Route::post('/register', [UserController::class,'register'])->middleware('guest');
 
-Route::post('/login', [UserController::class,'login']);
-Route::post('/logout', [UserController::class,'logout']);
+Route::post('/login', [UserController::class,'login'])->middleware('guest');
+Route::post('/logout', [UserController::class,'logout'])->middleware('mustBeLoggedIn');
 
 //Blog Routes
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
-Route::post('/create-post', [PostController::class, 'storeNewPost']);
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware('mustBeLoggedIn');
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('mustBeLoggedIn');
+Route::get('/post/{post}', [PostController::class, 'viewSinglePost']);
+
+//Profile Related Routes
+Route::get('/profile/{user:username}', [UserController::class, 'profile']);
